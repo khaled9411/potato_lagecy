a1f0873e23539500b9a9840664e3a061aaf9344f
khaled9411 <41273571+khaled9411@users.noreply.github.com>
2023-02-08 17:49:55 +0200

    peo

--- Potato Legacy/Assets/fire.cs
+++ Potato Legacy/Assets/fire.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class fire : MonoBehaviour
 {
+    public GameObject bullet;
+    public float fireRate;
+    public float fireTime;
+    Transform firePoint;
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+        firePoint = transform.GetChild(1);
+        InvokeRepeating("Fire", fireTime, fireRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Fire()
+    {
+        Instantiate(bullet, firePoint.position,firePoint.rotation);
     }
 }
